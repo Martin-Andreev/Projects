7d77972a894bf24f9b390614573244bbb367131c
agrecy <martin_andreev7@mail.bg>
2014-11-04 19:47:41 +0200

    SoftGag

---
+++ SoftGag/scripts/votesCount.js
@@ -0,0 +1,394 @@
+var pic1LikesCount = 0;
+function pic1LikesClick() {
+    pic1LikesCount += 1;
+    document.getElementById("pic1Likes").innerHTML = pic1LikesCount;
+}
+
+var pic1DislikeCount = 0;
+function pic1DislikesClick() {
+    pic1DislikeCount += 1;
+    document.getElementById("pic1Dislikes").innerHTML = pic1DislikeCount;
+}
+
+var pic2LikesCount = 0;
+function pic2LikesClick() {
+    pic2LikesCount += 1;
+    document.getElementById("pic2Likes").innerHTML = pic2LikesCount;
+}
+
+var pic2DislikesCount = 0;
+function pic2DislikesClick() {
+    pic2DislikesCount += 1;
+    document.getElementById("pic2Dislikes").innerHTML = pic2DislikesCount;
+}
+
+var pic3LikesCount = 0;
+function pic3LikesClick() {
+    pic3LikesCount += 1;
+    document.getElementById("pic3Likes").innerHTML = pic3LikesCount;
+}
+
+var pic3DislikeCount = 0;
+function pic3DislikesClick() {
+    pic3DislikeCount += 1;
+    document.getElementById("pic3Dislikes").innerHTML = pic3DislikeCount;
+}
+
+var pic4LikesCount = 0;
+function pic4LikesClick() {
+    pic4LikesCount += 1;
+    document.getElementById("pic4Likes").innerHTML = pic4LikesCount;
+}
+
+var pic4DislikeCount = 0;
+function pic4DislikesClick() {
+    pic4DislikeCount += 1;
+    document.getElementById("pic4Dislikes").innerHTML = pic4DislikeCount;
+}
+
+var pic5LikesCount = 0;
+function pic5LikesClick() {
+    pic5LikesCount += 1;
+    document.getElementById("pic5Likes").innerHTML = pic5LikesCount;
+}
+
+var pic5DislikeCount = 0;
+function pic5DislikesClick() {
+    pic5DislikeCount += 1;
+    document.getElementById("pic5Dislikes").innerHTML = pic5DislikeCount;
+}
+
+var cSharpLikesCount = 0;
+function cSharpLikesClick() {
+    cSharpLikesCount += 1;
+    document.getElementById("cSharpLikes").innerHTML = cSharpLikesCount;
+}
+
+var cSharpDislikeCount = 0;
+function cSharpDislikesClick() {
+    cSharpDislikeCount += 1;
+    document.getElementById("cSharpDislikes").innerHTML = cSharpDislikeCount;
+}
+
+var beardLikesCount = 0;
+function beardLikesClick() {
+    beardLikesCount += 1;
+    document.getElementById("beardLikes").innerHTML = beardLikesCount;
+}
+
+var beardDislikeCount = 0;
+function beardDislikesClick() {
+    beardDislikeCount += 1;
+    document.getElementById("beardDislikes").innerHTML = beardDislikeCount;
+}
+
+var loveLikesCount = 0;
+function loveLikesClick() {
+    loveLikesCount += 1;
+    document.getElementById("loveLikes").innerHTML = loveLikesCount;
+}
+
+var loveDislikeCount = 0;
+function loveDislikesClick() {
+    loveDislikeCount += 1;
+    document.getElementById("loveDislikes").innerHTML = loveDislikeCount;
+}
+
+
+
+var likesCount = 0;
+function likesClick() {
+    likesCount += 1;
+    document.getElementById("clicks").innerHTML = likesCount;
+}
+var dislikesCount = 0;
+function dislikeClick() {
+    dislikesCount += 1;
+    document.getElementById("dislikes").innerHTML = dislikesCount;
+}
+
+
+
+var hulkLikesCount = 0;
+function hulkLikesClick() {
+    hulkLikesCount += 1;
+    document.getElementById("hulkLikes").innerHTML = hulkLikesCount;
+}
+var hulkDislikesCount = 0;
+function hulkDislikesClick() {
+    hulkDislikesCount += 1;
+    document.getElementById("hulkDislikes").innerHTML = hulkDislikesCount;
+}
+
+
+
+var bakovLikesCount = 0;
+function bakovLikesClick() {
+    bakovLikesCount += 1;
+    document.getElementById("bakovLikes").innerHTML = bakovLikesCount;
+}
+var bakovDislikesCount = 0;
+function bakovDislikesClick() {
+    bakovDislikesCount += 1;
+    document.getElementById("bakovDislikes").innerHTML = bakovDislikesCount;
+}
+
+var svetlinatorLikesCount = 0;
+function svetlinatorLikesClick() {
+    svetlinatorLikesCount += 1;
+    document.getElementById("svetlinatorLikes").innerHTML = svetlinatorLikesCount;
+}
+var svetlinatorDislikesCount = 0;
+function svetlinatorDislikesClick() {
+    svetlinatorDislikesCount += 1;
+    document.getElementById("svetlinatorDislikes").innerHTML = svetlinatorDislikesCount;
+}
+
+var westsideLikesCount = 0;
+function westsideLikesClick() {
+    westsideLikesCount += 1;
+    document.getElementById("westsideLikes").innerHTML = westsideLikesCount;
+}
+var westsideDislikesCount = 0;
+function westsideDislikesClick() {
+    westsideDislikesCount += 1;
+    document.getElementById("westsideDislikes").innerHTML = westsideDislikesCount;
+}
+
+var naximusLikesCount = 0;
+function naximusLikesClick() {
+    naximusLikesCount += 1;
+    document.getElementById("naximusLikes").innerHTML = naximusLikesCount;
+}
+var naximusDislikesCount = 0;
+function naximusDislikesClick() {
+    naximusDislikesCount += 1;
+    document.getElementById("naximusDislikes").innerHTML = naximusDislikesCount;
+}
+
+
+var censoredLikesCount = 0;
+function censoredLikesClick() {
+    censoredLikesCount += 1;
+    document.getElementById("censoredLikes").innerHTML = censoredLikesCount;
+}
+var censoredDislikesCount = 0;
+function censoredDislikesClick() {
+    censoredDislikesCount += 1;
+    document.getElementById("censoredDislikes").innerHTML = censoredDislikesCount;
+}
+
+var angryLikesCount = 0;
+function angryLikesClick() {
+    angryLikesCount += 1;
+    document.getElementById("angryLikes").innerHTML = angryLikesCount;
+}
+var angryDislikesCount = 0;
+function angryDislikesClick() {
+    angryDislikesCount += 1;
+    document.getElementById("angryDislikes").innerHTML = angryDislikesCount;
+}
+
+var bondLikesCount = 0;
+function bondLikesClick() {
+    bondLikesCount += 1;
+    document.getElementById("bondLikes").innerHTML = bondLikesCount;
+}
+var bondDislikesCount = 0;
+function bondDislikesClick() {
+    bondDislikesCount += 1;
+    document.getElementById("bondDislikes").innerHTML = bondDislikesCount;
+}
+
+var montanaLikesCount = 0;
+function montanaLikesClick() {
+    montanaLikesCount += 1;
+    document.getElementById("montanaLikes").innerHTML = montanaLikesCount;
+}
+var montanaDislikesCount = 0;
+function montanaDislikesClick() {
+    montanaDislikesCount += 1;
+    document.getElementById("montanaDislikes").innerHTML = montanaDislikesCount;
+}
+
+var castAwayLikesCount = 0;
+function castAwayLikesClick() {
+    castAwayLikesCount += 1;
+    document.getElementById("castAwayLikes").innerHTML = castAwayLikesCount;
+}
+var castAwayDislikesCount = 0;
+function castAwayDislikesClick() {
+    castAwayDislikesCount += 1;
+    document.getElementById("castAwayDislikes").innerHTML = castAwayDislikesCount;
+}
+
+var ramboLikesCount = 0;
+function ramboLikesClick() {
+    ramboLikesCount += 1;
+    document.getElementById("ramboLikes").innerHTML = ramboLikesCount;
+}
+var ramboDislikesCount = 0;
+function ramboDislikesClick() {
+    ramboDislikesCount += 1;
+    document.getElementById("ramboDislikes").innerHTML = ramboDislikesCount;
+}
+
+var cheechLikesCount = 0;
+function cheechLikesClick() {
+    cheechLikesCount += 1;
+    document.getElementById("cheechLikes").innerHTML = cheechLikesCount;
+}
+var cheechDislikesCount = 0;
+function cheechDislikesClick() {
+    cheechDislikesCount += 1;
+    document.getElementById("cheechDislikes").innerHTML = cheechDislikesCount;
+}
+
+var superdidoLikesCount = 0;
+function superdidoLikesClick() {
+    superdidoLikesCount += 1;
+    document.getElementById("superdidoLikes").innerHTML = superdidoLikesCount;
+}
+var superdidoDislikesCount = 0;
+function superdidoDislikesClick() {
+    superdidoDislikesCount += 1;
+    document.getElementById("superdidoDislikes").innerHTML = superdidoDislikesCount;
+}
+
+
+var moneyLikesCount = 0;
+function moneyLikesClick() {
+    moneyLikesCount += 1;
+    document.getElementById("moneyLikes").innerHTML = moneyLikesCount;
+}
+var moneyDislikesCount = 0;
+function moneyDislikesClick() {
+    moneyDislikesCount += 1;
+    document.getElementById("moneyDislikes").innerHTML = moneyDislikesCount;
+}
+
+var nakovScoresLikesCount = 0;
+function nakovScoresLikesClick() {
+    nakovScoresLikesCount += 1;
+    document.getElementById("nakovScoresLikes").innerHTML = nakovScoresLikesCount;
+}
+var nakovScoresDislikesCount = 0;
+function nakovScoresDislikesClick() {
+    nakovScoresDislikesCount += 1;
+    document.getElementById("nakovScoresDislikes").innerHTML = nakovScoresDislikesCount;
+}
+
+var machineLikesCount = 0;
+function machineLikesClick() {
+    machineLikesCount += 1;
+    document.getElementById("machineLikes").innerHTML = machineLikesCount;
+}
+var machineDislikesCount = 0;
+function machineDislikesClick() {
+    machineDislikesCount += 1;
+    document.getElementById("machineDislikes").innerHTML = machineDislikesCount;
+}
+
+var wolverineLikesCount = 0;
+function wolverineLikesClick() {
+    wolverineLikesCount += 1;
+    document.getElementById("wolverineLikes").innerHTML = wolverineLikesCount;
+}
+var wolverineDislikesCount = 0;
+function wolverineDislikesClick() {
+    wolverineDislikesCount += 1;
+    document.getElementById("wolverineDislikes").innerHTML = wolverineDislikesCount;
+}
+
+var jungleLikesCount = 0;
+function jungleLikesClick() {
+    jungleLikesCount += 1;
+    document.getElementById("jungleLikes").innerHTML = jungleLikesCount;
+}
+var jungleDislikesCount = 0;
+function jungleDislikesClick() {
+    jungleDislikesCount += 1;
+    document.getElementById("jungleDislikes").innerHTML = jungleDislikesCount;
+}
+
+var kombatLikesCount = 0;
+function kombatLikesClick() {
+    kombatLikesCount += 1;
+    document.getElementById("kombatLikes").innerHTML = kombatLikesCount;
+}
+var kombatDislikesCount = 0;
+function kombatDislikesClick() {
+    kombatDislikesCount += 1;
+    document.getElementById("kombatDislikes").innerHTML = kombatDislikesCount;
+}
+
+var passwordLikesCount = 0;
+function passwordLikesClick() {
+    passwordLikesCount += 1;
+    document.getElementById("passwordLikes").innerHTML = passwordLikesCount;
+}
+var passwordDislikesCount = 0;
+function passwordDislikesClick() {
+    passwordDislikesCount += 1;
+    document.getElementById("passwordDislikes").innerHTML = passwordDislikesCount;
+}
+
+
+var talkLikesCount = 0;
+function talkLikesClick() {
+    talkLikesCount += 1;
+    document.getElementById("talkLikes").innerHTML = talkLikesCount;
+}
+var talkDislikesCount = 0;
+function talkDislikesClick() {
+    talkDislikesCount += 1;
+    document.getElementById("talkDislikes").innerHTML = talkDislikesCount;
+}
+
+var spartaLikesCount = 0;
+function spartaLikesClick() {
+    spartaLikesCount += 1;
+    document.getElementById("spartaLikes").innerHTML = spartaLikesCount;
+}
+var spartaDislikesCount = 0;
+function spartaDislikesClick() {
+    spartaDislikesCount += 1;
+    document.getElementById("spartaDislikes").innerHTML = spartaDislikesCount;
+}
+
+
+var errorLikesCount = 0;
+function errorLikesClick() {
+    errorLikesCount += 1;
+    document.getElementById("errorLikes").innerHTML = errorLikesCount;
+}
+var errorDislikesCount = 0;
+function errorDislikesClick() {
+    errorDislikesCount += 1;
+    document.getElementById("errorDislikes").innerHTML = errorDislikesCount;
+}
+
+
+var girlLikesCount = 0;
+function girlLikesClick() {
+    girlLikesCount += 1;
+    document.getElementById("girlLikes").innerHTML = girlLikesCount;
+}
+var girlDislikesCount = 0;
+function girlDislikesClick() {
+    girlDislikesCount += 1;
+    document.getElementById("girlDislikes").innerHTML = girlDislikesCount;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
